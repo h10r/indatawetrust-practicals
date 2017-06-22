d94420abd678a1974d6a5a54bccaa5d4de372b4c
Hendrik Heuer <hendrikheuer@gmail.com>
2017-06-22 16:48:51 +0200

    generate gephi

---
+++ 2017-06-22-petition-network-crawling/2_generate_gephi/generate_gephi_file.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+node_header = "nodedef> name VARCHAR, label VARCHAR" # , location VARCHAR"
+edge_header = "edgedef> user_to_name VARCHAR, user_from_name VARCHAR"
+
+user_follows = defaultdict(dict)
+
+for connection in open("petition_followers.tsv").readlines():
+	connection_parts = connection.strip().split("\t")
+
+	if len( connection_parts ) == 3:
+		user_to, user_from, location_from = connection_parts
+		user_to = user_to.replace("@", "")
+
+		#print user_to, user_from, location_from
+		user_follows[ user_to.strip() ][ user_from.strip() ] = location_from.strip()
+
+	elif len( connection_parts ) == 2:
+		user_to, user_from = connection_parts
+		user_to = user_to.replace("@", "")
+
+		user_follows[ user_to.strip() ][ user_from.strip() ] = "Unknown"
+
+print node_header
+
+for username, following in user_follows.items():
+	print username, ", ", username
+
+print edge_header
+
+for username, following in user_follows.items():
+	for follower_username, follower_location in following.items():
+		print username, ",", follower_username
