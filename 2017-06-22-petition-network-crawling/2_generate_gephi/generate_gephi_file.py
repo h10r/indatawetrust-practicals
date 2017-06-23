2783ddbe6b0c760fd34236fd2dbc31cb0c7bd022
Hendrik Heuer <hendrikheuer@gmail.com>
2017-06-23 14:03:14 +0200

    updated script for gephi file

--- 2017-06-22-petition-network-crawling/2_generate_gephi/generate_gephi_file.py
+++ 2017-06-22-petition-network-crawling/2_generate_gephi/generate_gephi_file.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 node_header = "nodedef> name VARCHAR, label VARCHAR, followers DOUBLE" # , location VARCHAR"
 edge_header = "edgedef> user_to_name VARCHAR, user_from_name VARCHAR"
 
+user_exists = {}
 user_follows = defaultdict(dict)
 
 for connection in open("../1_get_followers/petition_users_followers.tsv").readlines():
@@ -15,16 +16,26 @@ for connection in open("../1_get_followers/petition_users_followers.tsv").readli
 		#print user_to, user_from, location_from
 		user_follows[ user_to.strip() ][ user_from.strip() ] = location_from.strip()
 
+		user_exists[ user_to.strip() ] = True
+		user_exists[ user_from.strip() ] = True
+
 	elif len( connection_parts ) == 2:
 		user_to, user_from = connection_parts
 		user_to = user_to.replace("@", "")
 
 		user_follows[ user_to.strip() ][ user_from.strip() ] = "Unknown"
 
+		user_exists[ user_to.strip() ] = True
+		user_exists[ user_from.strip() ] = True
+
 print node_header
 
-for username, following in user_follows.items():
-	print username, ",", username, ",", str( len(following) )
+for username in user_exists.keys():
+
+	if username in user_follows:
+		print username, ",", username, "," , str( len( user_follows[ username ] ) )
+	else:
+		print username, ",", username, ",", str(0)
 
 print edge_header
 
